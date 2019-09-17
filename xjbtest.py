add2ad78f24603af8685051b90d6f5a5323ed9ad
Xufangwei <ddcw_xufangwei@163.com>
2019-09-17 16:38:51 +0800

    learn to clone project

--- xjbtest.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-r = requests.get("http://www.xjistedu.cn/")
-r.encoding = r.apparent_encoding
-
-soup = bs(r.text)
-for trr in soup.findAll('div',{'class':'wrapper'}):
-    print("class标签描述" +trr.text)
